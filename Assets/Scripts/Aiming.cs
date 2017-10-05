9755cd583c7b32ea23eacb24bb39e67003233958
Unknown <ring.miles@gmail.com>
2017-10-04 20:49:42 -0500

    Initial project v2

---
+++ Assets/Scripts/Aiming.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aiming : MonoBehaviour {
+
+
+	public Vector3 aimDownSight;
+	private Vector3 aimHipSight;
+	private Vector3 endPos;
+	private bool aiming;
+	public float aimSpeed;
+
+
+	// Use this for initialization
+	void Start () {
+		//0, 0, 0
+		aimHipSight = transform.localPosition;
+		//ak
+		//aimDownSight = new Vector3 (0.2f, 0.075f, -0.1f);
+		//m4 (0, 0.035, -0.1)
+		//pistol (-0.1, 0.1, -0.1)
+		//sniper (0.05, 0.05, -0.1)
+
+		aiming = false;
+		endPos = aimHipSight;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		bool previousAim = aiming;
+		if (Input.GetMouseButtonDown (1)) {
+			aiming = !aiming;
+			if (aiming) {
+				endPos = aimDownSight;
+			} else {
+				endPos = aimHipSight;
+			}
+		}
+			
+		transform.localPosition = Vector3.Slerp (transform.localPosition, endPos, Time.deltaTime * aimSpeed);
+	}
+}
---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour {
+
+	public float damage = 10;
+	public float range = 100f;
+	public float fireRate = 15f;
+	public float impactForce = 30f;
+
+	public int maxAmmo = 10;
+	private int currentAmmo;
+	public float reloadTime = 1f;
+	private bool isReloading;
+
+	public Camera fpsCam;
+	public GameObject muzzleFlash;
+	public GameObject impactEffect; 
+
+	public Animator animator;
+
+	private float nextTimeToFire = 0f;
+	// Use this for initialization
+	void Start () {
+		currentAmmo = maxAmmo;
+		isReloading = false;
+	}
+
+	void OnEnable(){
+		isReloading = false;
+		animator.SetBool ("Reloading", false);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		if (Input.GetMouseButton (0) && Time.time >= nextTimeToFire && currentAmmo > 0 && !isReloading) {
+			nextTimeToFire = Time.time + 1f/fireRate;
+			Shoot ();
+		}
+
+
+		if(Input.GetKeyDown(KeyCode.R) && !isReloading){
+			StartCoroutine(Reload());
+		}
+	}
+
+	void FixedUpdate(){
+
+
+	}
+		
+
+
+	void Shoot(){
+		animator.Play ("WeaponShoot");
+		RaycastHit hit;
+
+		currentAmmo--;
+		//GameObject t_muzzleFlash = Instantiate (muzzleFlash);
+		//Destroy (t_muzzleFlash, 0.3f);
+		if (Physics.Raycast (fpsCam.transform.position, fpsCam.transform.forward, 
+			   out hit, range)) {
+			Debug.Log (hit.transform.name);
+
+			Target target = hit.transform.GetComponent<Target> ();
+			if (target != null) {
+				target.TakeDamage (damage);
+			}
+
+			if (hit.rigidbody != null) {
+				hit.rigidbody.AddForce (-hit.normal * impactForce);
+			}
+			GameObject impactGO = Instantiate (impactEffect, hit.point, Quaternion.LookRotation (hit.normal));
+			Destroy (impactGO, 0.2f);
+		}
+	}
+
+	IEnumerator Reload(){
+		isReloading = true;
+
+		animator.SetBool ("Reloading", true);
+
+		//0.25f for animation transitions
+		yield return new WaitForSeconds (reloadTime- 0.25f);
+		animator.SetBool ("Reloading", false);
+		yield return new WaitForSeconds (0.25f);
+		currentAmmo = maxAmmo;
+		isReloading = false;
+	}
+}
